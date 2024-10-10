8fc3f187bdefcd5c956fc183e6974d8b8430c5cf
CommitPending <reagancarter51@yahoo.com>
2024-10-10 17:45:49 -0500

    update route

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
 
 const FullLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // This will run whenever the route changes
+    console.log("Route changed: ", location.pathname);
+  }, [location]);
+
   return (
     <main>
       <Header />
