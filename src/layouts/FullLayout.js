42d2c20995b169e40c12b313a9295461a279f025
CommitPending <reagancarter51@yahoo.com>
2024-10-10 19:07:22 -0500

    Update FullLayout

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
 
 const FullLayout = () => {
-  const [hash, setHash] = useState(window.location.hash);
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      setHash(window.location.hash);
-      console.log("Hash changed: ", window.location.hash);
-    };
-
-    window.addEventListener("hashchange", handleHashChange);
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
   return (
     <main>
       <Header />
